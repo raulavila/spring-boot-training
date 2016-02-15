ca2f58e899cbfeee2a740a64eab41f909c4588fe
Raul Avila <ravila@pivotal.io>
2016-02-15 14:19:37 +0000

    Add Spring Initializr with web, mysql and jpa
    Signed-off-by: Josh Hill <jhill@pivotal.io>

---
+++ src/main/java/io/pivotal/training/SpringRestTrainingApplication.java
@@ -0,0 +1,12 @@
+package io.pivotal.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestTrainingApplication.class, args);
+	}
+}
