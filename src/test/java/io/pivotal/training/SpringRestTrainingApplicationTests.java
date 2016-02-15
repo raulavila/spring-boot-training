a96135e612a8b9589fd8cb4d954d388c172bf46a
Raul Avila <ravila@pivotal.io>
2016-02-15 14:35:47 +0000

    Inject web application context into test class
    Signed-off-by: Josh Hill <jhill@pivotal.io>

--- src/test/java/io/pivotal/training/SpringRestTrainingApplicationTests.java
+++ src/test/java/io/pivotal/training/SpringRestTrainingApplicationTests.java
@@ -2,14 +2,19 @@ package io.pivotal.training;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SpringRestTrainingApplication.class)
 @WebAppConfiguration
 public class SpringRestTrainingApplicationTests {
+
+	@Autowired
+	WebApplicationContext webApplicationContext;
 
 	@Test
 	public void contextLoads() {
