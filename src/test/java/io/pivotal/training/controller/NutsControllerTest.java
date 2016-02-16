438dd46f0fe8eca4342d22e8c60366b6e18934c7
Raul Avila <ravila@pivotal.io>
2016-02-16 17:59:32 +0000

    Add standalone test for nuts controller
    Signed-off-by: Josh Hill <jhill@pivotal.io>

---
+++ src/test/java/io/pivotal/training/controller/NutsControllerTest.java
@@ -0,0 +1,53 @@
+package io.pivotal.training.controller;
+
+import io.pivotal.training.errors.NutNotFoundException;
+import io.pivotal.training.service.NutsService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MockServletContext.class)
+@WebAppConfiguration
+public class NutsControllerTest {
+
+    private MockMvc mvc;
+
+    @Mock
+    NutsService nutsService;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        mvc = MockMvcBuilders.standaloneSetup(new NutsController(nutsService))
+                    .setControllerAdvice(new ErrorHandler())
+                    .build();
+    }
+
+    @Test
+    public void test() throws Exception {
+        when(nutsService.get(42L)).thenThrow(new NutNotFoundException("Could not find nut with id: 42"));
+
+        mvc.perform(get("/nuts/42").accept(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.message", is("Could not find nut with id: 42")));
+    }
+
+}
